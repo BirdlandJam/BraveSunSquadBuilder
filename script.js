1658a04981f7f9e7a5a831e71d8f00f4a22a84c9
BirdlandJam <143234255+BirdlandJam@users.noreply.github.com>
2023-08-27 19:49:01 -0700

    Update and rename app.js to script.js - App V2

---
+++ script.js
@@ -0,0 +1,48 @@
+// Initialize totalPoints to 0
+let totalPoints = 0;
+
+// Parse the CSV into an array of ship objects
+function csvToArray(csv) {
+  const [header, ...rows] = csv.split('\n').map(row => row.split(','));
+  return rows.map(row => Object.fromEntries(row.map((value, i) => [header[i], value])));
+}
+
+// Update the total points for the fleet
+function updateTotalPoints(cost, isAdding) {
+  totalPoints += isAdding ? parseInt(cost) : -parseInt(cost);
+  document.getElementById('totalPoints').innerText = totalPoints;
+}
+
+// Fetch CSV and Populate Available Ships
+fetch('ships.csv')
+  .then(response => response.text())
+  .then(csvContent => {
+    const availableShips = document.getElementById('availableShips');
+    const fleet = document.getElementById('fleet');
+
+    const ships = csvToArray(csvContent);
+
+    // Populate available ships
+    ships.forEach(ship => {
+      const shipBox = document.createElement('div');
+      shipBox.className = 'ship-box';
+      shipBox.innerHTML = `
+        <p><strong>Ship:</strong> ${ship.Name}</p>
+        <p><strong>Class:</strong> ${ship.Class}</p>
+        <p><strong>Ability:</strong> ${ship.Ability}</p>
+        <p><strong>Cost:</strong> ${ship.Cost}</p>
+        <button>Add to Fleet</button>
+      `;
+      shipBox.querySelector('button').addEventListener('click', () => {
+        const newShipBox = shipBox.cloneNode(true);
+        newShipBox.querySelector('button').innerText = "Remove from Fleet";
+        newShipBox.querySelector('button').addEventListener('click', () => {
+          newShipBox.remove();
+          updateTotalPoints(ship.Cost, false);
+        });
+        fleet.appendChild(newShipBox);
+        updateTotalPoints(ship.Cost, true);
+      });
+      availableShips.appendChild(shipBox);
+    });
+  });
