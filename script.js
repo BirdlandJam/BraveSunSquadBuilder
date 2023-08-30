356b92265752f8bb5e432f60ec7dd7e619b65acb
BirdlandJam <143234255+BirdlandJam@users.noreply.github.com>
2023-08-30 08:40:59 -0700

    Update script.js - Add class menus

--- script.js
+++ script.js
@@ -19,35 +19,71 @@ console.log("Fetch initiated");
 fetch('https://birdlandjam.github.io/BraveSunSquadBuilder/ships.csv')
   .then(response => response.text())
   .then(csvContent => {
-    console.log(csvContent);
     const availableShips = document.getElementById('availableShips');
     const fleet = document.getElementById('fleet');
-
     const ships = csvToArray(csvContent);
-    console.log(ships)
-    
-    // Populate available ships
+
+    // Create an object to hold ships by class
+    const shipsByClass = {};
+
+    // Group ships by class
     ships.forEach(ship => {
-      const shipBox = document.createElement('div');
-      shipBox.className = 'ship-box';
-      shipBox.innerHTML = `
-        <p><strong>#</strong> ${ship.ShipNumber}</p>
-        <p><strong>Ship:</strong> ${ship.Name}</p>
-        <p><strong>Class:</strong> ${ship.Class}</p>
-        <p><strong>Ability:</strong> ${ship.Ability}</p>
-        <p><strong>Cost:</strong> ${ship.Cost}</p>
-        <button>Add to Fleet</button>
-      `;
-      shipBox.querySelector('button').addEventListener('click', () => {
-        const newShipBox = shipBox.cloneNode(true);
-        newShipBox.querySelector('button').innerText = "Remove from Fleet";
-        newShipBox.querySelector('button').addEventListener('click', () => {
-          newShipBox.remove();
-          updateTotalPoints(ship.Cost, false);
-        });
-        fleet.appendChild(newShipBox);
-        updateTotalPoints(ship.Cost, true);
-      });
-      availableShips.appendChild(shipBox);
+      if (!shipsByClass[ship.Class]) {
+        shipsByClass[ship.Class] = [];
+      }
+      shipsByClass[ship.Class].push(ship);
     });
+
+    // Create and populate collapsible menus for each class
+    Object.keys(shipsByClass).forEach(shipClass => {
+      const classContainer = document.createElement('div');
+      const button = document.createElement('button');
+      button.innerText = shipClass;
+      button.className = 'collapsible';
+      classContainer.appendChild(button);
+
+      const contentContainer = document.createElement('div');
+      contentContainer.className = 'content';
+
+      shipsByClass[shipClass].forEach(ship => {
+        const shipBox = document.createElement('div');
+        shipBox.className = 'ship-box';
+        shipBox.innerHTML = `
+          <p><strong>#</strong> ${ship.ShipNumber}</p>
+          <p><strong>Ship:</strong> ${ship.Name}</p>
+          <p><strong>Class:</strong> ${ship.Class}</p>
+          <p><strong>Ability:</strong> ${ship.Ability}</p>
+          <p><strong>Cost:</strong> ${ship.Cost}</p>
+          <button>Add to Fleet</button>
+        `;
+        shipBox.querySelector('button').addEventListener('click', () => {
+          const newShipBox = shipBox.cloneNode(true);
+          newShipBox.querySelector('button').innerText = "Remove from Fleet";
+          newShipBox.querySelector('button').addEventListener('click', () => {
+            newShipBox.remove();
+            updateTotalPoints(ship.Cost, false);
+          });
+          fleet.appendChild(newShipBox);
+          updateTotalPoints(ship.Cost, true);
+        });
+        contentContainer.appendChild(shipBox);
+      });
+
+      classContainer.appendChild(contentContainer);
+      availableShips.appendChild(classContainer);
+    });
+
+    // Collapsible functionality
+    const coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
   });
