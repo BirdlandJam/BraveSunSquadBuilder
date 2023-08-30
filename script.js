15898b24d210258a122fc68231ff7f05cd173ea0
BirdlandJam <143234255+BirdlandJam@users.noreply.github.com>
2023-08-30 09:55:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -79,10 +79,10 @@ fetch('https://birdlandjam.github.io/BraveSunSquadBuilder/ships.csv')
       coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         const content = this.nextElementSibling;
-        if (content.style.display === "block") {
+        if (content.style.display === "flex") {
           content.style.display = "none";
         } else {
-          content.style.display = "block";
+          content.style.display = "flex";
         }
       });
     }
